e259509141dca677bb0f5e444d86df72eadaa1f7
“00pe <408445889@qq.com>
2017-12-22 07:42:26 +0800

    接口测试页面js

--- bupt-openiot-application/src/main/resources/static/js/request.js
+++ bupt-openiot-application/src/main/resources/static/js/request.js
@@ -1,11 +1,11 @@
 function request_click(){
 	var request_method = $("#request_method").val();
-	var params =  $("#request_params").val();
+	var params =  {};
 	debugger;
 	$.ajax({
         type:$("#request_method").val() ,     //post
         url:  $("#requst_url").val(),     //url 后台会告诉你
-        data:JSON.stringify(params),
+        data:params,
         success:function (data) {   //如果成功获取
         	console.log(data);
         },
